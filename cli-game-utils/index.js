c78358dea6eed0c9324fdcea44fb7dd638b8c532
Todd Knight <knight.todd.04@gmail.com>
2019-03-27 11:30:47 -0700

    implemented sPrint and added sleep

--- cli-game-utils/index.js
+++ cli-game-utils/index.js
@@ -1,3 +1,26 @@
-exports.sPrint = function(input) {
-	console.log("This function will eventually print out strings passed in to it sequentially.");
+exports.sPrint = function(input, delay) {
+	if (typeof delay == "undefined") {charDelay = 100}
+	else {charDelay = delay};
+	var arr = [];
+ 	for (i = 0; i < input.length; i++){
+
+ 		arr[i]=input[i];
+			
+		}
+	arr.forEach(function(element){
+
+		process.stdout.write(element);
+		exports.sleep(charDelay);
+
+	});
+	console.log('');
+}
+
+exports.sleep = function(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
 }
---
+++ cli-game-utils/main.js
@@ -0,0 +1,2 @@
+const util = require("./index.js");
+util.sPrint("Hello World", 500);
